ea0cf7c0e3c99cfb2b4eaba3bec0a7b82b0f0381
João Paulo <95300143+jpcchaves@users.noreply.github.com>
2022-05-12 11:33:14 -0300

    estrutura base

---
+++ calculadora/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Calculator  from './main/Calculator.jsx';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <Calculator />, document.getElementById('root')
+  );
+
+reportWebVitals();
